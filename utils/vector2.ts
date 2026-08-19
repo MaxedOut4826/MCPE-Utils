150de2371c24af8390a5695383aa90a62ffa12b7
MaxedOut4826 <maximreed@hotmail.com>
2026-08-19 20:25:36 +0100

    first commit

---
+++ utils/vector2.ts
@@ -0,0 +1,128 @@
+export type Vector2 = {
+    x: number;
+    y: number;
+};
+
+export class Vector2Utils {
+    static add(vector1: Vector2, vector2: Vector2): Vector2 {
+        const { x: x0, y: y0 } = vector1;
+        const { x: x1, y: y1 } = vector2;
+
+        return {
+            x: x0 + x1,
+            y: y0 + y1,
+        };
+    }
+
+    static subtract(vector1: Vector2, vector2: Vector2): Vector2 {
+        const { x: x0, y: y0 } = vector1;
+        const { x: x1, y: y1 } = vector2;
+
+        return {
+            x: x0 - x1,
+            y: y0 - y1,
+        };
+    }
+
+    static multiply(vector: Vector2, scalar: number): Vector2 {
+        const { x, y } = vector;
+
+        return {
+            x: x * scalar,
+            y: y * scalar,
+        };
+    }
+
+    static hadamard_product(vector1: Vector2, vector2: Vector2): Vector2 {
+        const { x: x0, y: y0 } = vector1;
+        const { x: x1, y: y1 } = vector2;
+
+        return {
+            x: x0 * x1,
+            y: y0 * y1,
+        };
+    }
+
+    static normalise(vector: Vector2, scalar: number): Vector2 {
+        const { x: x0, y: y0 } = vector;
+
+        return {
+            x: x0 / scalar,
+            y: y0 / scalar,
+        };
+    }
+
+    static areaOf(vector: Vector2): number {
+        const { x, y } = vector;
+
+        return x * y;
+    }
+
+    static distance_sqr(vector1: Vector2, vector2: Vector2): number {
+        const { x: x0, y: y0 } = vector1;
+        const { x: x1, y: y1 } = vector2;
+
+        const dx = x0 - x1;
+        const dy = y0 - y1;
+
+        return dx * dx + dy * dy;
+    }
+
+    static distance(vector1: Vector2, vector2: Vector2): number {
+        const { x: x0, y: y0 } = vector1;
+        const { x: x1, y: y1 } = vector2;
+
+        const dx = x0 - x1;
+        const dy = y0 - y1;
+
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    static lerp(vector1: Vector2, vector2: Vector2, t: number): Vector2 {
+        const { x: x0, y: y0 } = vector1;
+        const { x: x1, y: y1 } = vector2;
+
+        const dx = x1 - x0;
+        const dy = y1 - y0;
+
+        return {
+            x: dx * t + x0,
+            y: dy * t + y0,
+        };
+    }
+
+    static get_lowest_vector(vector1: Vector2, vector2: Vector2): Vector2 {
+        const { x: x0, y: y0 } = vector1;
+        const { x: x1, y: y1 } = vector2;
+
+        return {
+            x: Math.min(x0, x1),
+            y: Math.min(y0, y1),
+        };
+    }
+
+    static get_highest_vector(vector1: Vector2, vector2: Vector2): Vector2 {
+        const { x: x0, y: y0 } = vector1;
+        const { x: x1, y: y1 } = vector2;
+
+        return {
+            x: Math.max(x0, x1),
+            y: Math.max(y0, y1),
+        };
+    }
+
+    static to_string(vector: Vector2): string {
+        const { x, y } = vector;
+
+        return `${x}, ${y}`;
+    }
+
+    static round(vector: Vector2, precision: number): Vector2 {
+        const { x, y } = vector;
+
+        return {
+            x: parseFloat(x.toFixed(precision)),
+            y: parseFloat(y.toFixed(precision)),
+        };
+    }
+}
---
+++ utils/vector3.ts
@@ -0,0 +1,141 @@
+export type Vector3 = {
+    x: number;
+    y: number;
+    z: number;
+};
+
+export class Vector3Utils {
+    static add(vector1: Vector3, vector2: Vector3): Vector3 {
+        const { x: x0, y: y0, z: z0 } = vector1;
+        const { x: x1, y: y1, z: z1 } = vector2;
+
+        return {
+            x: x0 + x1,
+            y: y0 + y1,
+            z: z0 + z1,
+        };
+    }
+
+    static subtract(vector1: Vector3, vector2: Vector3): Vector3 {
+        const { x: x0, y: y0, z: z0 } = vector1;
+        const { x: x1, y: y1, z: z1 } = vector2;
+
+        return {
+            x: x0 - x1,
+            y: y0 - y1,
+            z: z0 - z1,
+        };
+    }
+
+    static multiply(vector: Vector3, scalar: number): Vector3 {
+        const { x, y, z } = vector;
+
+        return {
+            x: x * scalar,
+            y: y * scalar,
+            z: z * scalar,
+        };
+    }
+
+    static hadamard_product(vector1: Vector3, vector2: Vector3): Vector3 {
+        const { x: x0, y: y0, z: z0 } = vector1;
+        const { x: x1, y: y1, z: z1 } = vector2;
+
+        return {
+            x: x0 * x1,
+            y: y0 * y1,
+            z: z0 * z1,
+        };
+    }
+
+    static normalise(vector: Vector3, scalar: number): Vector3 {
+        const { x: x0, y: y0, z: z0 } = vector;
+
+        return {
+            x: x0 / scalar,
+            y: y0 / scalar,
+            z: z0 / scalar,
+        };
+    }
+
+    static volumeOf(vector: Vector3): number {
+        const { x, y, z } = vector;
+
+        return x * y * z;
+    }
+
+    static distance_sqr(vector1: Vector3, vector2: Vector3): number {
+        const { x: x0, y: y0, z: z0 } = vector1;
+        const { x: x1, y: y1, z: z1 } = vector2;
+
+        const dx = x0 - x1;
+        const dy = y0 - y1;
+        const dz = z0 - z1;
+
+        return dx * dx + dy * dy + dz * dz;
+    }
+
+    static distance(vector1: Vector3, vector2: Vector3): number {
+        const { x: x0, y: y0, z: z0 } = vector1;
+        const { x: x1, y: y1, z: z1 } = vector2;
+
+        const dx = x0 - x1;
+        const dy = y0 - y1;
+        const dz = z0 - z1;
+
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    static lerp(vector1: Vector3, vector2: Vector3, t: number): Vector3 {
+        const { x: x0, y: y0, z: z0 } = vector1;
+        const { x: x1, y: y1, z: z1 } = vector2;
+
+        const dx = x1 - x0;
+        const dy = y1 - y0;
+        const dz = z1 - z0;
+
+        return {
+            x: dx * t + x0,
+            y: dy * t + y0,
+            z: dz * t + z0,
+        };
+    }
+
+    static get_lowest_vector(vector1: Vector3, vector2: Vector3): Vector3 {
+        const { x: x0, y: y0, z: z0 } = vector1;
+        const { x: x1, y: y1, z: z1 } = vector2;
+
+        return {
+            x: Math.min(x0, x1),
+            y: Math.min(y0, y1),
+            z: Math.min(z0, z1),
+        };
+    }
+
+    static get_highest_vector(vector1: Vector3, vector2: Vector3): Vector3 {
+        const { x: x0, y: y0, z: z0 } = vector1;
+        const { x: x1, y: y1, z: z1 } = vector2;
+
+        return {
+            x: Math.max(x0, x1),
+            y: Math.max(y0, y1),
+            z: Math.max(z0, z1),
+        };
+    }
+
+    static to_string(vector: Vector3): string {
+        const { x, y, z } = vector;
+
+        return `${x}, ${y}, ${z}`;
+    }
+
+    static round(vector: Vector3, precision: number): Vector3 {
+        const { x, y, z } = vector;
+
+        return {
+            x: parseFloat(x.toFixed(precision)),
+            y: parseFloat(y.toFixed(precision)),
+            z: parseFloat(z.toFixed(precision)),
+        };
+    }
+}
